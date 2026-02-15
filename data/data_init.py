49103c30aa5577bfa970a6e7fb5222bc60e58c29
Yanke Li <liyankeyan@gmail.com>
2026-02-15 13:30:52 +0100

    Implement data_processing for dataset handling
    Add data_processing function to process multiple datasets.

--- data/data_init.py
+++ data/data_init.py
@@ -1,1 +1,18 @@
+from data_utils import *
 
+def data_processing():
+    process_func_map = {
+        'P12': process_p12data,
+        'P19': process_p19data,
+        'MIMICIII': process_mimiciii,
+    }
+    for data_name in ['P12', 'P19', 'MIMICIII']:
+        data_path = './data/rawdata/'+data_name
+        os.makedirs(os.path.dirname("./data/processed_data/"+data_name+"/"), exist_ok=True)
+        processed_data = process_func_map[data_name](data_path)
+        print('parsed data:', len(processed_data))
+
+
+if __name__ == '__main__':
+    data_processing()
+    print('done')
